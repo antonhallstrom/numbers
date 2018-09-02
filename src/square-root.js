2e80f28a74e813aa2ccf7c42d865ad1305ca5102
Anton Hallstrom <antonhallstrom@live.se>
2018-09-02 16:29:59 +0200

    fixup! feat: create squareRoot

--- src/square-root.js
+++ src/square-root.js
@@ -2,7 +2,7 @@
  * Compute square roots by Newton`s method
  * whenever we have a guess, y, for the value
  * of the square root of a number, x, we can
- * perform a simple manimulation to get a better
+ * perform a simple manipulation to get a better
  * guess, by averaging, y, with x / y.
  */
 
@@ -24,6 +24,7 @@ function isNumber(x) {
 }
 
 let previousGuess = 0
+// limited precision
 let tolerance = 0.001
 
 function improve(guess, number) {
@@ -39,7 +40,7 @@ function goodEnough(guess, number) {
 }
 
 function squareRoot(guess, number) {
-  if (isNumber(guess) && isNumber(number)) {
+  if (!isNumber(guess) && !isNumber(number)) {
     throw new Error('Arguments to squareRoot must be of type Number.')
   }
 
@@ -47,6 +48,44 @@ function squareRoot(guess, number) {
     return guess
   } else {
     previousGuess = guess
+    console.log(previousGuess)
     return squareRoot(average(guess, improve(guess, number)), number)
+  }
+}
+
+// cuberoot, iterator, is recursive, the procedure is defined in terms of itself.
+
+/** A decomposition of cubeRoot
+ *         cubeRoot
+ *         /     \
+ * goodEnough  improve
+ *    /   \        |
+ *   abs  pow    average
+ *
+ *
+ * e.g we are able to regard pow as a "black-box", we are not at any moment conserned
+ * about how pow procedure computes it's result.
+ * input -> [black box] -> output
+ * A user should not have to know how the procedure is implemented in order to use it.
+ */
+function improve(guess, number) {
+  return ((number / Math.pow(guess, 2)) + 2 * guess) / 3
+}
+
+function goodEnough(guess, number) {
+  return Math.abs(Math.pow(guess, 3) - number) / guess < tolerance
+}
+
+function cubeRoot(guess, number) {
+  if (!isNumber(guess) && !isNumber(number)) {
+    throw new Error('Arguments to squareRoot must be of type Number.')
+  }
+
+  if (goodEnough(guess, number)) {
+    return guess
+  } else {
+    previousGuess = guess
+    console.log(previousGuess)
+    return cubeRoot(improve(guess, number), number)
   }
 }
